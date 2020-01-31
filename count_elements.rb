e07c2d4fd6d57ec634bb84abfbaff713724a3e89
Gavrilajava <georgy.gavrilchik@gmail.com>
2020-01-31 11:50:20 -0600

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
 def count_elements(array)
   # code goes here
+  myhash = {}
+  array.each{ |element|
+    if myhash.has_key?(element)
+      myhash[element] += 1
+    else 
+      myhash[element] = 1
+    end
+  }
+  myhash
 end
  
